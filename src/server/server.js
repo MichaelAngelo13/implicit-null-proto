b1657e751f06c04bfb16c63f2990a6b8d29cb953
Michael Garcia <angelo@Michaels-Air.grandenetworks.net>
2023-02-15 23:13:00 -0600

    great progress, 2/3 mvp features done

--- src/server/server.js
+++ src/server/server.js
@@ -11,10 +11,32 @@ const PORT = 3000;
  * handle parsing request body
  */
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // to open a new page send a get request to an extension that will then serve a new html
 // we can use the sendFile method available from our response and path to create an absolute path to go to new html files
-app.use('/nice', quoteController.niceTest, (req, res) => res.sendFile(path.join(__dirname, '../client/html-pages/nice.html')))
+app.use('/nice', quoteController.niceTest, (req, res) => res.sendFile(path.join(__dirname, '../client/html-pages/nice.html')));
+
+// need route to handle creation of original quote
+// ? strangely this route would not work using a use method, had to be post
+// adds quotes to our quote collection
+app.post('/addQuote', quoteController.addQuote, (req, res) => {
+res.status(200).json(res.locals.newQuote)
+})
+
+// handles creation of api quote
+app.post('/addStrangerQuote', quoteController.addStrangerQuote, (req, res) => {
+  res.status(200).json(res.locals.newStrangerQuote)
+  })
+
+// to handle deletion
+
+// and to handle getting(will be initiated by the front end mostlikely)
+app.get('/savedQuotes', quoteController.getQuotes, (req, res) => {
+  res.status(200).json(res.locals.quotes)
+})
+
+// lastly updating
 
 
 
